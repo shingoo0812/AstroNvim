c3262732e21a3732a5bd369998f5e1e63f269fd6
shingoo0812 <shingoo0812@gmail.com>
2024-03-17 14:11:43 +0900

    ADD TODO

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -1,14 +1,5 @@
+-- TODO: hoge
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
   {
     "github/copilot.vim",
     lazy = false,
@@ -35,4 +26,13 @@ return {
     "jesseduffield/lazygit",
     event = "VeryLazy",
   },
+  {
+     "folke/todo-comments.nvim",
+     lazy = false,
+     dependencies = { "nvim-lua/plenary.nvim" },
+     config = function()
+       require("todo-comments").setup()
+     end,
+  },
+
 }
