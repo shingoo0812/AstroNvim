1b885e07b64da717aa407c726022a7a1cdf9c31b
shingoo0812 <shingoo0812@gmail.com>
2024-03-17 20:05:25 +0900

    Add catppuccin

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -1,38 +1,31 @@
--- TODO: hoge
 return {
   {
     "github/copilot.vim",
     lazy = false,
   },
   {
-    "kylechui/nvim-surround",
-    version = "*", -- Une for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-  },
-  {
     "ggandor/leap.nvim",
-    event = "VeryLazy",
-  },
-  {
     "mg979/vim-visual-multi",
+    "jesseduffield/lazygit",
+    "kylechui/nvim-surround",
     event = "VeryLazy",
   },
-   {
-    "jesseduffield/lazygit",
-    event = "VeryLazy",
+  { "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
   },
   {
-     "folke/todo-comments.nvim",
-     lazy = false,
-     dependencies = { "nvim-lua/plenary.nvim" },
-     config = function()
-       require("todo-comments").setup()
-     end,
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+--    config = function()
+--      require("todo-comments").setup()
+--    end,
+    opts = {},
+    event = "User AstroFile",
+    keys = {
+     { "<leader>T", "", desc = "TODO" },
+     { "<leader>Tl", "<cmd>TodoLocList<cr>", desc = "Open TODOLockList." },
+     { "<leader>Tt", "<cmd>TodoTelescope<cr>", desc = "Open TODO in Telescope." },
+    },
   },
-
 }
