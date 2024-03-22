647cc3e32a5e67d4ce38b11af0aa8a578194ae95
shingoo0812 <shingoo0812@gmail.com>
2024-03-22 20:34:14 +0900

    modify copilot

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -1,40 +1,31 @@
 return {
   {
-    "github/copilot.vim",
-    lazy = false,
-  },
-  {
-    "ggandor/leap.nvim",
     "mg979/vim-visual-multi",
     "jesseduffield/lazygit",
     event = "VeryLazy",
   },
   {
-      "kylechui/nvim-surround",
-      version = "*", -- Use for stability; omit to use `main` branch for the latest features
-      event = "VeryLazy",
-      config = function()
-          require("nvim-surround").setup({
-              -- Configuration here, or leave empty to use defaults
-          })
-      end
-  },
-  { "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-  },
-  {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
---    config = function()
---      require("todo-comments").setup()
---    end,
+    --    config = function()
+    --      require("todo-comments").setup()
+    --    end,
     opts = {},
     event = "User AstroFile",
     keys = {
-     { "<leader>T", "", desc = "TODO" },
-     { "<leader>Tl", "<cmd>TodoLocList<cr>", desc = "Open TODOLockList." },
-     { "<leader>Tt", "<cmd>TodoTelescope<cr>", desc = "Open TODO in Telescope." },
+      { "<leader>T", "", desc = "TODO" },
+      { "<leader>Tl", "<cmd>TodoLocList<cr>", desc = "Open TODOLockList." },
+      { "<leader>Tt", "<cmd>TodoTelescope<cr>", desc = "Open TODO in Telescope." },
     },
+  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   config = function() require("copilot").setup {} end,
+  -- },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function() require("copilot_cmp").setup() end,
   },
 }
